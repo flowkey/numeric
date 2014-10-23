b1c9297da24e0ad1768f759e7ea0f6b6b33974be
Ahmed Hassan <a.hassan@buismed.de>
2014-10-23 21:28:18 +0200

    inital commit

---
+++ README.md
@@ -0,0 +1,4 @@
+#Numeric
+library allows you to perform sophisticated numerical computations in pure javascript in the browser and elsewhere.
+
+for documentation visit: http://www.numericjs.com/
---
+++ package.js
@@ -0,0 +1,14 @@
+Package.describe({
+  name: 'flowkey:numeric',
+  summary: 'Sophisticated numerical computations in pure javascript',
+  version: '1.0.0',
+  git: 'https://github.com/flowkey/numeric.git'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('METEOR@0.9.4')
+  api.addFiles('flowkey:numeric.js');
+  api.export("numeric");
+});
+
+
